5ad79d8ae289bdc3826efe11f7494bcb781b5476
Michael Wen <pme100bos06@outlook.com>
2020-06-12 14:59:01 +0800

    145. Outsourcing the Route Configuration 147. Protecting routes with canActivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import {AuthService} from './auth.service';
+import {AuthGuard} from './auth-guard.service';
 
-const appRoutes: Routes =  [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-    ] },
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
-    ] },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found' }
-  // Make sure that this wild card route is at the bottom of all routes !!!
-];
 
 @NgModule({
   declarations: [
@@ -42,9 +31,9 @@ const appRoutes: Routes =  [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
